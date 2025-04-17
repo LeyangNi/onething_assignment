e30fc68b0c0dd0f9583954c4a3c56c87e81a5c05
Leyang Ni <15662630+leyangnig-ni@user.noreply.gitee.com>
2025-04-16 17:56:35 -0700

    modified version

---
+++ todo-app/components/TaskItem.tsx
@@ -0,0 +1,46 @@
+// import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { Task } from '../types/task';
+import { Link } from 'expo-router';
+
+interface Props {
+  task: Task;
+  onToggle: () => void;
+  onDelete: () => void;
+}
+
+export default function TaskItem({ task, onToggle, onDelete }: Props) {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={onToggle} style={{ flex: 1 }}>
+        <Text style={task.completed ? styles.completed : undefined}>
+          {task.title}
+        </Text>
+      </TouchableOpacity>
+      {/* <Link href={`/(tabs)/task/${task.id}`} asChild>
+        <Text style={styles.link}>📝</Text>
+      </Link> */}
+      <TouchableOpacity onPress={onDelete}>
+        <Text style={styles.link}>Delete</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    paddingVertical: 10,
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderColor: '#ccc',
+  },
+  completed: {
+    textDecorationLine: 'line-through',
+    color: 'gray',
+  },
+  link: {
+    fontSize: 18,
+    marginHorizontal: 8,
+  },
+});
